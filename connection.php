3c10ae0396a6fd5be324ad0a72a5d22202845923
samiNCL <60569888+samiNCL@users.noreply.github.com>
2020-03-17 02:20:52 +0000

    Update connection.php
    if there was an error in connection there will be a message to tell that.

--- connection.php
+++ connection.php
@@ -5,8 +5,13 @@
 /*This is a new connection to the local host */
     // you have to obtain the database firstly. then you have to change the connection details to suite your machine.
     // address , user , password , name_DB
- $conn = new mysqli("127.0.0.1", "root", "sami1989", "sagefeedback");
-
+ $conn = new mysqli("127.0.01", "root", "sami1989", "sagefeedback");
+    // Check connection
+    if ($conn->connect_error) {
+        die("<br><h1> Hi!, <br> Connection failed: <br></h1><h2>" . $conn->connect_error);
+    }
+    echo "<h3> Connected successfully<br></h3>";
 ?>
+
 
 
